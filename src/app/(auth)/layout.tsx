f83b145f679f66cb689b4ad4b3bf0a95f92dbce1
wawle <berkayyucel96@hotmail.com>
2023-08-05 16:15:14 +0300

    first commit

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,45 @@
+import { ClerkProvider } from "@clerk/nextjs/app-beta";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { dark } from "@clerk/themes";
+import Topbar from "@/components/shared/top-bar";
+import LeftSidebar from "@/components/shared/left-sidebar";
+import RightSidebar from "@/components/shared/right-sidebar";
+import Bottombar from "@/components/shared/bottom-bar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
